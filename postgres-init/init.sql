7d50c28e521b155da73b2e4724241642c5b1ff92
Athos Danilo <athosdanilomarques@gmail.com>
2026-07-02 12:39:00 -0300

    Rodar Localmente

---
+++ postgres-init/init.sql
@@ -0,0 +1,7 @@
+-- Criação dos bancos de dados locais para cada microsserviço
+CREATE DATABASE banco_autenticacao;
+CREATE DATABASE banco_catalogo;
+CREATE DATABASE banco_emprestimos;
+CREATE DATABASE banco_reservas;
+CREATE DATABASE banco_notificacoes;
+CREATE DATABASE banco_recomendacoes;
